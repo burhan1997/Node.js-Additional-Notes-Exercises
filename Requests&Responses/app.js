5904337c36dc788fe58fd70e101e046ee44ed653
burhan1997 <burhanelaldi97@gmail.com>
2024-02-09 16:29:17 +0100

    middleware s added

--- Requests&Responses/app.js
+++ Requests&Responses/app.js
@@ -6,6 +6,21 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+//middleware
+app.use((req, res, next) => {
+console.log('new request made:');
+console.log('host: ', req.hostname);
+console.log('path: ', req.path);
+console.log('method: ', req.method);
+next();
+});
+
+app.use((req, res, next) => {
+console.log('in the next middleware');
+next();
+});
+
+
 app.get('/', (req, res) => {
   //res.send('<p>home page</p>');
   res.sendFile('./views/index.html', { root: __dirname });
